0c9198b14e1e63d07c4b3e6f7cef127a43a4236c
eugene <eugenecoop14@gmail.com>
2024-09-30 13:30:45 +0300

    cookie fix

--- app/api/supabase-fetch/route.ts
+++ app/api/supabase-fetch/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers'
 
 export async function GET() {
   try {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
+    const supabase = createClient();
 
     const { data, error } = await supabase
       .from('scrapeditems')
